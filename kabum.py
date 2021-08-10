1c164a6f8caf9493c8f60662ee9caea91073b4a9
Morelatto <morelatto@pm.me>
2021-08-10 20:57:06 -0300

    Updating script

--- kabum.py
+++ kabum.py
@@ -1,11 +1,11 @@
 """Script to download products from kabum.com.br
 Usage:
-  kabum.py <category_url>...
+  kabum.py <category>...
   kabum.py --list-categories
   kabum.py -h | --help | --version
 
 Arguments:
-  <category_url>...         Category urls to download. E.g.: https://www.kabum.com.br/hardware/placa-de-video-vga
+  <category>...         Category to download. E.g.: hardware, tv
 
 Options:
   --list-categories     List all product categories.
@@ -13,94 +13,108 @@ Options:
   --version             Show version.
 """
 import requests
-
 from docopt import docopt
-from parsel import Selector
 from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
 
 from kabum.spiders.kabum import KabumSpider
-
-KABUM = 'https://www.kabum.com.br/'
 
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
                   'Chrome/73.0.3683.86 Safari/537.36 '
 }
 
+CATEGORIES_URL = 'https://servicespub.prod.api.aws.grupokabum.com.br/categoria/v1/categoria'
+ACTIVE_PARAM = '?ativo=1'
+
 
 def fetch_categories():
-    cats = {}
-    r = requests.get(KABUM, headers=headers)
+    categories = {}
+    r = requests.get(CATEGORIES_URL + ACTIVE_PARAM, headers=headers)
     if r.status_code == 200:
-        sel = Selector(text=r.text)
-        category = None
-        for section in sel.css('#menu_left div'):
-            class_name = section.xpath("@class").get()
-            if class_name == 'icone_categoria':
-                category = section.css('.h2titcategoria::text').get()
-            elif class_name == 'texto_categoria':
-                sub_category = section.css('p a')
-                titles = sub_category.css('::text').getall()
-                urls = sub_category.css('::attr(href)').getall()
-                cats[category] = dict(zip(titles, urls))
+        res = r.json()
+        for cat in res['categorias']:
+            categories[cat['nome']] = cat['amigavel']
     else:
-        print(f'Got {r.status_code} response code from {KABUM}')
-    return cats
+        print(f'Got {r.status_code} response code fetching categories')
+    return categories
 
 
-def execute(urls):
-    process = CrawlerProcess(get_project_settings())
-    process.crawl(KabumSpider, start_urls=urls)
-    process.start()
-    process.stop()
-
-
-def get_category(all_cats):
-    for i, cat in enumerate(all_cats):
+def get_category(categories):
+    print("=" * 9)
+    print("CATEGORY")
+    print("=" * 9)
+    for i, cat in enumerate(categories):
         print(i + 1, cat)
     while True:
         try:
             n = input('> Enter category number: ')
-            cat_list = list(all_cats)
+            cat_list = list(categories)
             n = int(n)
-            if 0 < n < len(cat_list):
-                category = cat_list[n - 1]
+            if 0 < n <= len(cat_list):
+                category = categories[cat_list[n - 1]]
                 return category
             else:
                 raise Exception
+        except KeyboardInterrupt:
+            return
         except:
             print('Invalid category number')
 
 
-def get_sub_category(all_sub_cats):
-    for i, sub_category in enumerate(all_sub_cats):
+def fetch_subcategories(sub_category):
+    sub_categories = {}
+    r = requests.get(f'{CATEGORIES_URL}/{sub_category}' + ACTIVE_PARAM, headers=headers)
+    if r.status_code == 200:
+        res = r.json()
+        for sub_cat in res['categorias']:
+            sub_categories[sub_cat['nome']] = sub_cat['amigavel']
+    else:
+        print(f'Got {r.status_code} response code fetching sub categories')
+    return sub_categories
+
+
+def get_sub_category(sub_categories):
+    print("=" * 12)
+    print("SUB CATEGORY")
+    print("=" * 12)
+    for i, sub_category in enumerate(sub_categories):
         print(i + 1, sub_category)
     while True:
         try:
             n = input('> Enter subcategory number: ')
-            sub_cat_list = list(all_sub_cats)
+            sub_cat_list = list(sub_categories)
             n = int(n)
-            if 0 < n < len(sub_cat_list):
-                return sub_cat_list[n - 1]
+            if 0 < n <= len(sub_cat_list):
+                return sub_categories[sub_cat_list[n - 1]]
             else:
                 raise Exception
+        except KeyboardInterrupt:
+            return
         except:
             print('Invalid subcategory number')
 
 
+def execute(urls):
+    process = CrawlerProcess(get_project_settings())
+    process.crawl(KabumSpider, categories=urls)
+    process.start()
+    process.stop()
+
+
 def main():
-    urls = []
-    if args['<category_url>']:
-        urls = args['<category_url>']
+    if args['<category>']:
+        execute(args['<category>'])
     elif args['--list-categories']:
-        all_cats = fetch_categories()
-        cat = get_category(all_cats)
-        sub_cat = get_sub_category(all_cats[cat])
-        urls = [all_cats[cat][sub_cat]]
-    execute(urls)
+        categories = fetch_categories()
+        if categories:
+            category = get_category(categories)
+            sub_categories = fetch_subcategories(category)
+            if sub_categories:
+                sub_category = get_sub_category(sub_categories)
+                execute(f'{category}/{sub_category}')
 
 
 if __name__ == '__main__':
-    args = docopt(__doc__, version='0.2')
+    args = docopt(__doc__, version='0.3')
     main()
